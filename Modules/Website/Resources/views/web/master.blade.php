41786590df6b4405d4df8b322bb6c2626e7cc991
programmerlab <kroy.iips@gmail.com>
2018-09-19 04:36:25 +0530

    gapi

--- Modules/Website/Resources/views/web/master.blade.php
+++ Modules/Website/Resources/views/web/master.blade.php
@@ -38,7 +38,8 @@
 <!-- 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
  -->
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ <script src="{{asset('public/js/jquery.min.js')}}"></script> 
+     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> -->
 
     
 </head>
@@ -187,7 +188,7 @@ $country_flag = $fcountry['flag'];
     @yield('js')
     @stack('scripts')
          <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-   <!--  <script src="{{asset('public/js/jquery.min.js')}}"></script> 
+   <!--  
     -->  <!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script-->
 
 
