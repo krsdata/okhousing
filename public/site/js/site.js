a88b5cc255af2e3a1e370960565aed2cfcc92f26
programmerlab <kroy.iips@gmail.com>
2018-09-24 04:52:50 +0530

    map popup

--- public/site/js/site.js
+++ public/site/js/site.js
@@ -335,15 +335,15 @@ $('body').on('click', '#property_enquiry_btn', function(e) {
                     $('#showHideMap').show(); 
                     $("#Mymap").hide();
                     $('.PropertyDetailsOnMap').show();    
-                    //$("#PropertyDetails").html(response.html);
+                    $("#PropertyDetails").html(response.html);
                     $('#PropertyDetailsOnMap').html(response.html)
-                    
+                    $('.close-popup').hide();
                     $('#showHideMap').attr('href','#ShowMap');
                     $('#showHideMap').html('Show Map');
 
                     var href= $('#showHideMap').attr('href');
 
-                    $("#OpenPropertyDetailModal").trigger("click");
+                     $("#OpenPropertyDetailModal").trigger("click");
                         
                      $("span#Count_pro_"+id).text(response.pass_pro_count);
                      $('.nav-tabs > li.active > a').trigger('click');
